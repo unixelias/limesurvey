3906249b226af475c15722769ae14a3121f40605
Elias Alves <unixelias@gmail.com>
2017-03-03 10:46:13 -0300

    Corrige build

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ shopt -s extglob
 #for i in $( ls docker); do
 #  case "$i" in
 #       *-test )  ;;
-#       * ) docker build -t unixelias/Limesurvey:$i docker/$i ;;
+#       * ) docker build -t unixelias/limesurvey:$i docker/$i ;;
 #  esac
 #done
 
-docker build -t unixelias/Limesurvey:0.0.1-dev docker/dev
+docker build -t unixelias/limesurvey:0.0.1-dev docker/dev
