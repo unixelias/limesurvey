e706b3f9fe9570e65ca04ef5a2a41040c96d72d9
Elias Alves <unixelias@gmail.com>
2017-06-14 09:47:05 -0300

    Update travis push system

--- build.sh
+++ build.sh
@@ -4,11 +4,10 @@ shopt -s extglob
 for i in $( ls docker); do
   case "$i" in
        *-test )  ;;
-       * ) docker build -t unixelias/limesurvey:$i docker/$i ;;
+       * ) docker build -t unixelias/limesurvey:$i docker/$i && docker push unixelias/limesurvey:$i ;;
   esac
 done
 
 
 #Exclusivo para DEV version
 #docker build -t unixelias/limesurvey:dev docker/dev
-
