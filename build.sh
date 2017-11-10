8c49fac815b12de575ed3330bac633a22a08e1f3
Elias Alves <unixelias@gmail.com>
2017-11-10 20:40:27 -0200

    Atualiza versão para  2.72.4
    Modifica compilação da versão latest.

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ for i in $( ls docker); do
   esac
 done
 
+docker tag unixelias/limesurvey:2.72.4 unixelias/limesurvey:latest && docker push unixelias/limesurvey:latest
 
 #Exclusivo para DEV version
 #docker build -t unixelias/limesurvey:dev docker/dev
-
