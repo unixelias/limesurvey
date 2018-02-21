937b5554ca70164d7f0e7fe8b852ca4ac71a2053
Elias Alves <unixelias@gmail.com>
2018-02-21 20:00:11 -0300

    Tag adjusts

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ shopt -s extglob
 
 docker build -t unixelias/limesurvey:${TRAVIS_BRANCH} \
                    --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
-                   --build-arg BRANCHE_VCS=${TRAVIS_BRANCH} \
+                   --build-arg VCS_BRANCH=${TRAVIS_BRANCH} \
                    --build-arg VCS_REF=`git rev-parse --short HEAD` \
                    --build-arg LS_VERSION=`git ls-remote --tags https://github.com/LimeSurvey/LimeSurvey.git | awk '{print $2}' | grep -v '{}' | awk -F"/" '{print $3}' | sort -n -t. -k1,1 -k2,2 -k3,3 | tail -n 1`\
                    docker;                   
